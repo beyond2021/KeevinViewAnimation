fbcb5fbeec48cbce012dd3745cac1065293bd98f
“KEEVIN <keevin.mitchell@gmail.com>
2015-02-10 20:29:18 -0500

    keyframe animations

--- KeevinViewAnimations/ExampleIIViewController.swift
+++ KeevinViewAnimations/ExampleIIViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ExampleIIViewController: UIViewController {
     
+    /*
+    Here we create an image view and add it to the main view. We then call imageFadeIn() which creates a second view with a different image. We add this view above the first image view and set its alpha to 0. In the animation block, we animate its alpha value, making it visible. We then use a completion closure to set the image view’s image to the second image and we remove the second image view from the view hierarchy since it is no longer needed. I’ve added a long delay so that the animation doesn’t happen right at the moment we select Example II from the table view. The duration is also a bit long so we can see what’s going on in the demo.
+    */
+    
     override func viewWillAppear(animated: Bool) {
         
         let firstImageView = UIImageView(image: UIImage(named: "bg01.png"))
