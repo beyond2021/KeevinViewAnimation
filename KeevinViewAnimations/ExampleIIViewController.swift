b5e2df22b179092b89f7aea4f55b787ab5aba4c8
“KEEVIN <keevin.mitchell@gmail.com>
2015-02-10 20:19:38 -0500

    replace the background image of a view automatically when some action happens

--- KeevinViewAnimations/ExampleIIViewController.swift
+++ KeevinViewAnimations/ExampleIIViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class ExampleIIViewController: UIViewController {
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        let firstImageView = UIImageView(image: UIImage(named: "bg01.png"))
+        firstImageView.frame = view.frame
+        view.addSubview(firstImageView)
+        
+        imageFadeIn(firstImageView)
+        
+    }
+    
+    func imageFadeIn(imageView: UIImageView) {
+        
+        let secondImageView = UIImageView(image: UIImage(named: "bg02.png"))
+        secondImageView.frame = view.frame
+        secondImageView.alpha = 0.0
+        
+        view.insertSubview(secondImageView, aboveSubview: imageView)
+        
+        UIView.animateWithDuration(2.0, delay: 2.0, options: .CurveEaseOut, animations: {
+            secondImageView.alpha = 1.0
+            }, completion: {_ in
+                imageView.image = secondImageView.image
+                secondImageView.removeFromSuperview()
+        })
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
