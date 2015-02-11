a893667d2c939fcce81a82ee99d498a7553ee6c7
“KEEVIN <keevin.mitchell@gmail.com>
2015-02-10 19:53:17 -0500

    Anime,ate text field when loaded

---
+++ KeevinViewAnimations/ExampleIViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ExampleIViewController.swift
+//  KeevinViewAnimations
+//
+//  Created by KEEVIN MITCHELL on 2/10/15.
+//  Copyright (c) 2015 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class ExampleIViewController: UIViewController {
+    
+    @IBOutlet weak var centerAlignUsername: NSLayoutConstraint!
+    @IBOutlet weak var centerAlignPassword: NSLayoutConstraint!
+    //@IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func login(sender: UIButton) {
+        
+        let bounds = self.loginButton.bounds
+        UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: nil, animations: {
+            self.loginButton.bounds = CGRect(x: bounds.origin.x - 20, y: bounds.origin.y, width: bounds.size.width + 60, height: bounds.size.height)
+            self.loginButton.enabled = false
+            }, completion: nil)
+        
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        centerAlignUsername.constant -= view.bounds.width
+        centerAlignPassword.constant -= view.bounds.width
+        loginButton.alpha = 0.0
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            self.centerAlignUsername.constant += self.view.bounds.width
+            self.centerAlignPassword.constant += self.view.bounds.width
+            self.loginButton.alpha = 1
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        
+    }
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
